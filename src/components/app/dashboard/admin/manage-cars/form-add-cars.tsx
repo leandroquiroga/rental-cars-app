2a17734d2faad72ab9cc194aeabd7b2fbbb0dad2
leandroquiroga <leandroquiroga9514@gmail.com>
2025-04-05 21:15:29 -0300

    Se crea nuevo componente para refactorizar el formulario y su drag and drop

--- src/components/app/dashboard/admin/manage-cars/form-add-cars.tsx
+++ src/components/app/dashboard/admin/manage-cars/form-add-cars.tsx
@@ -1,5 +1,5 @@
  "use client"
-import React, { useState } from 'react'
+import React, { Dispatch } from 'react'
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -15,18 +15,22 @@ import { z } from "zod"
 import { useForm } from "react-hook-form"
 import { formSchema, resolveTheme } from '@/utils/functions'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
-import { UploadDropzone } from '@/utils/uploadthing'
 import { useTheme } from 'next-themes'
-import { cn } from '@/lib/utils'
+import UploadDropZone from './upload-drop-zone'
+import { LoaderCircle } from 'lucide-react'
+import { toast } from 'sonner'
+import axios from 'axios'
 
 
 export type FormFieldName =  "name" | "cv" | "transmission" | "people" | "photo" | "engine" | "type" | "priceDay" | "isPublish"
 
+interface FormAddCardProps {
+  setOpenDialog: Dispatch<React.SetStateAction<boolean>>;
+}
 
-export const FormAddCard = () => {
-  const [photoUpload, setPhotoUpload] = useState<boolean>(false);
+export const FormAddCard = ({setOpenDialog}: FormAddCardProps) => {
   const { theme, systemTheme } = useTheme();
-  const effectiveTheme = resolveTheme(theme!, systemTheme!);
+  const effectiveTheme: "dark" | "light" = resolveTheme(theme!, systemTheme!) as "dark" | "light";
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     
@@ -41,23 +45,21 @@ export const FormAddCard = () => {
       priceDay: "",
       type: "",
     },
+    mode: "onChange"
   })
 
-  const uploadConfig = {
-    endpoint: "photo" as const,
-     
-    onClientUploadComplete: (res: any) => {
-      form.setValue("photo", res[0].url); 
-      setPhotoUpload(true);
-    },
-    onUploadError: (error: Error) => {
-      console.log(`ERROR! ${error.message}`);
-      setPhotoUpload(false);
-    },
-  };
   const handleOnSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values)
+    setOpenDialog(false);
+    try {
+      await axios.post("/api/cars", values)
+      toast.success("Car created successfully", {description: "The car has been created successfully" ,duration: 5000, position: "bottom-right"})
+    } catch (error) {
+      console.log(error)
+      toast.error("Something went wrong", {description: "Please try again", duration: 5000, position: "bottom-right"})
+    }
   };
+
+  const { isValid, isSubmitSuccessful, isLoading } = form.formState;
 
   return (
     <Form {...form}>
@@ -202,37 +204,27 @@ export const FormAddCard = () => {
               <FormItem>
                 <FormLabel>Car Image</FormLabel>
                 <FormControl>
-                  {photoUpload
-                    ? (<p className='text-0'>Image Uploaded!</p>)
-                    : (
-                    <UploadDropzone
-                      className="w-full border-3 border-dotted rounded-lg p-2 transition-colors"
-                      appearance={{
-                        button({ ready, isUploading }) {
-                          return {
-                            backgroundColor: cn(effectiveTheme === "dark" ? "#efefef":"#202020"),
-                            fontSize: "1rem",
-                            ...(ready && { color: cn(effectiveTheme === "dark" ? "#202020":"#efefef") }),
-                            ...(isUploading && { color: cn(effectiveTheme === "dark" ? "#efefef":"#202020") }),
-                          };
-                        },
-                        container: {
-                          marginTop: "1rem",
-                        },
-                        allowedContent: {
-                          color: "#a1a1aa",
-                        },
-                      }}
-                        {...uploadConfig}
-                    />
-                  )}
+                  <UploadDropZone
+                    effectiveTheme={effectiveTheme}
+                    endpoint={"photo"}
+                    onUploadComplete={(url: string) =>
+                    {
+                      form.setValue("photo", url);
+                      form.trigger("photo")
+                    }}
+                    onUploadError={(error: Error) => { console.log(`ERROR! ${error.message}`) }}
+                    buttonText="Upload image"
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
         </div>
-        <Button className='w-full mt2' type="submit">Create Car</Button>
+        {
+          isLoading ? <LoaderCircle className='w-6 h-6 animate-spin' /> :
+          <Button className='w-full mt-2 cursor-pointer' type="submit" disabled={!isValid && !isSubmitSuccessful}>Create Car</Button>
+        }
       </form>
     </Form>
   )
