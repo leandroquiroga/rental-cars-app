f1d7261f5b2ec84501047c183a4b8d719ccd4841
leandroquiroga <leandroquiroga9514@gmail.com>
2025-04-10 20:26:58 -0300

    Se crea validacion para redirigir al usuario en caso de que no esté logueado

--- src/app/(shop)/dashboard/admin/manage-cars/page.tsx
+++ src/app/(shop)/dashboard/admin/manage-cars/page.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
-import { ButtonAddCars } from '@/components/app'
+import { ButtonAddCars, ListCars } from '@/components/app'
+import { redirect } from 'next/navigation'
+import { auth } from '@clerk/nextjs/server'
+// import prisma from '@/lib/db';
 
-export default function ManageCarsPage() {
+export default async function ManageCarsPage() {
+  const { userId } = await auth();
+
+  if (!userId) return redirect('/');
+
+  const cars: [] = []
+  // const cars = await prisma.car.findMany({where: { userid: userId }, orderBy: { createdAt: 'desc' }});
+
+  console.log({cars}, {userId});
   return (
-    <>
-      <div className="flex justify-between">
-        <h2 className='text-xl font-bold'>
+    <div className='h-screen'>
+      <div className="flex max-w-4xl mx-auto flex-col gap-4">
+        <h2 className='text-xl font-bold text-center'>
           Manage your Cars
         </h2>
+        {
+          !cars.length ? (<p> No hay autos disponibles </p> ) : <ListCars />      
+        }
+      </div>
+      <div className='block absolute bottom-10 right-5'>
         <ButtonAddCars />
       </div>
-    </>
+    </div>
   )
 }
