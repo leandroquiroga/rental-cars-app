d4dee5a39c9d6d19b7803598c820ecd68facff21
leandroquiroga <leandroquiroga9514@gmail.com>
2025-04-15 11:36:53 -0300

    Se corrije la UI del renderizados de los autos

--- src/components/app/dashboard/admin/manage-cars/list-cars.tsx
+++ src/components/app/dashboard/admin/manage-cars/list-cars.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 // import { Toaster } from '@/components/ui/sonner'
 // import { Fuel, Gauge, Gen, Trash, Users, Wrench } from 'lucide-react';
 import { Cars, useDataBase } from '@/hooks/useDatabase';
-import { SkeletonCars } from '@/components/app/Skeleton/SkeletonCars';
+import { SkeletonCars } from '@/components/app/skeleton/skeleton-cars';
 
 
 interface PropsListCars {
@@ -19,20 +19,22 @@ export const ListCars = ({userId}: PropsListCars) => {
   if (loading) return (<SkeletonCars />);
   if (error) return <div>Error</div>
   return (
-    <div className='grid grid-cols-2 gap-6 my-4 lg:grid-cols-3'>
+    <div className='grid grid-cols-1 md:grid-cols-2 gap-6 my-4 lg:grid-cols-3'>
       {
         response?.map((car: Cars) => (
           <div key={car.id} className='relative rounded-lg shadow-md hover:shadow-lg transition-all duration-300 ease-in-out'>
-            <Image
-              src={car.photo}
-              alt={car.name}
-              width={600}
-              height={600}
-              className='rounded-xl object-cover img-cars'
-            />
+
+            <div className="relative w-full h-48 lg:h-64">
+              <Image
+                src={car.photo}
+                alt={car.name}
+                fill
+                className='rounded-xl object-cover'
+              />
+            </div>
             {car.isPublished
-              ? <p className='absolute top-0 right-0 w-full p-2 text-center bg-green-300'>Published</p>
-              : <p className='absolute top-0 right-0 w-full p-2 text-center bg-red-300'>Not Published</p>}
+              ? <p className='absolute top-0 right-0 w-full p-2 text-center rounded-t-lg bg-green-300'>Published</p>
+              : <p className='absolute top-0 right-0 w-full p-2 text-center rounded-t-lg bg-red-300'>Not Published</p>}
 
             <div className='relative p-3'>
               <div className='flex flex-col mb-3 gap-x-4'>
