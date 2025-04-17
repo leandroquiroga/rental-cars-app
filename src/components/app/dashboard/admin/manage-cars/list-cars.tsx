bf19a103adbb5ace7dd8899b1a798ef998486bf1
leandroquiroga <leandroquiroga9514@gmail.com>
2025-04-17 11:25:37 -0300

    Se agrega descripcion de los auto a su card correspondiente

--- src/components/app/dashboard/admin/manage-cars/list-cars.tsx
+++ src/components/app/dashboard/admin/manage-cars/list-cars.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import Image from 'next/image';
 // import { Button } from '@/components/ui/button'
 // import { Toaster } from '@/components/ui/sonner'
-// import { Fuel, Gauge, Gen, Trash, Users, Wrench } from 'lucide-react';
+import { Fuel, Gauge, Gem, User, Wrench } from 'lucide-react';
 import { Cars, useDataBase } from '@/hooks/useDatabase';
 import { SkeletonCars } from '@/components/app/skeleton/skeleton-cars';
 
@@ -19,7 +19,7 @@ export const ListCars = ({userId}: PropsListCars) => {
   if (loading) return (<SkeletonCars />);
   if (error) return <div>Error</div>
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 gap-6 my-4 lg:grid-cols-3'>
+    <div className='grid grid-cols-1 md:grid-cols-2 md:my-2 gap-6 my-4 lg:grid-cols-3'>
       {
         response?.map((car: Cars) => (
           <div key={car.id} className='relative rounded-lg shadow-md hover:shadow-lg transition-all duration-300 ease-in-out'>
@@ -38,7 +38,37 @@ export const ListCars = ({userId}: PropsListCars) => {
 
             <div className='relative p-3'>
               <div className='flex flex-col mb-3 gap-x-4'>
-                <p>{car.name}</p>
+                <p className='text-xl lg:min-h-fit'>{car.name}</p>
+                <p className='text-md lg:min-h-fit'>${car.priceDay} /day</p>
+              </div>
+
+              <div className='grid md:grid-cols-2 gap-x-4'>
+
+                <p className='flex items-center capitalize'>
+                  <Gem className="h-4 w-4 mr-2" strokeWidth={1} />
+                  {car.type}
+                </p>      
+
+                <p className='flex items-center capitalize'>
+                  <Wrench className="h-4 w-4 mr-2" strokeWidth={1} />
+                  {car.transmission}
+                </p> 
+
+                <p className='flex items-center capitalize'>
+                  <User className="h-4 w-4 mr-2" strokeWidth={1} />
+                  {car.people}
+                </p> 
+
+                <p className='flex items-center capitalize'>
+                  <Fuel className="h-4 w-4 mr-2" strokeWidth={1} />
+                  {car.engine}
+                </p> 
+
+                <p className='flex items-center capitalize'>
+                  <Gauge className="h-4 w-4 mr-2" strokeWidth={1} />
+                  {car.cv} CV
+                </p> 
+
               </div>
             </div>
           
