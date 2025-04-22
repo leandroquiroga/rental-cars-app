4c791e6198ffecb311348af07ef156aecb18e3d7
leandroquiroga <leandroquiroga9514@gmail.com>
2025-04-22 15:29:48 -0300

    Se crea un archivo que gestiona todos los endpoint a la bases de datos

---
+++ src/services/service.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { z } from "zod";
+import { formSchema } from "@/utils/functions";
+
+type OrderBy = 'asc' | 'desc';
+type MethodType = 'findMany' | 'findUnique';
+export type Values = z.infer<typeof formSchema>
+
+
+export const findOneDatabase = async (method: MethodType, id: string) => {
+  try {
+    const url = `/api/cars?method=${method}&id=${id}`;
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error("[CARS]", error);
+    throw new Error('Error fetching cars');
+  };
+};
+
+export const findManyDatabase = async (method: MethodType, orderBy: OrderBy) => {
+  try {
+    const url = `/api/cars?method=${method}&orderBy=${orderBy}`;
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error("[CARS]", error);
+    throw new Error('Error fetching cars');
+  };
+}
+
+
+export const createDataBaseServices = async (values: Values) => {
+  try {
+    const url = `/api/cars`;
+    await axios.post(url, values);
+  } catch (error) {
+    console.error("[CARS]", error);
+    throw new Error('Error creating car');
+  };
+}
